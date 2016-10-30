d3a8647b48e2bf893a09756ae231b43dc0af824d
lydiadai <lyida0511@gmail.com>
2016-10-30 17:50:10 +0800

    implement wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
-  
+
   def show
   @job = Job.find(params[:id])
   end
@@ -11,6 +11,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title,:description)
+    params.require(:job).permit(:title,:description,:wage_upper_bound,:wage_lower_bound,:contact_email)
   end
 end
